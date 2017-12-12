c7b93b2d8cdce86292ed31e91f32134f2b0bc4b3
Leandro Manoel da Silva <mds2.design@gmail.com>
2017-12-12 16:15:26 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,19 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-var myArr = ['leandro', 32, true, 'Silva', null, 1.7, false];
+var myArr = ['leandro', true, function() {}, null, 1.7, false, {valor: 32}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 function myfunction(arr) {
 	return arr;
-};
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-console.log(myfunction(myArr)[2]); // true
+console.log(myfunction( myArr[1] ); // true
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -22,7 +22,10 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-console.log(returnArray(myArr,3)); // Silva
+function returnArray(arr, valor) {
+  return arr[valor];
+}
+console.log(returnArray(myArr,3)); // null
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
@@ -33,7 +36,11 @@ var mixedValuesArray = ['Curso', 33, true, undefined, null];
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-console.log(myfunction(mixedValuesArray)); // (5) ["Curso", 33, true, undefined, null]
+console.log(returnArray( mixedValuesArray, 0)); // 'Curso'
+console.log(returnArray( mixedValuesArray, 1)); // 33
+console.log(returnArray( mixedValuesArray, 2)); // true
+console.log(returnArray( mixedValuesArray, 3)); // undefined
+console.log(returnArray( mixedValuesArray, 4)); // null
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
