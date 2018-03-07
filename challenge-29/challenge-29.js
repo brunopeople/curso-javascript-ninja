7c4f55775c299482c468780e7c607d47de20e3b3
Leandro Manoel da Silva <mds2.design@gmail.com>
2018-03-07 15:46:24 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,10 +1,10 @@
-(function() {
+( function( win, doc, $ ) {
   'use strict';
 
   /*
   Vamos estruturar um pequeno app utilizando módulos.
   Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
+  A primeira etapa vai ser o cadastro de veículos, que deverá funcionar da
   seguinte forma:
   - No início do arquivo, deverá ter as informações da sua empresa - nome e
   telefone (já vamos ver como isso vai ser feito)
@@ -35,5 +35,77 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  var app = (function appController() {
+    return {
+      init: function init() {
+        console.log( 'app Init' );
+        this.companyInfo();
+        this.initEvents();
+      },
 
-})();
+      initEvents: function initEvents() {
+        $('[data-js="form-register"]').on( 'submit', this.handleSubmit ); 
+      },
+
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        console.log( 'submit' );
+        var $tableCar = $( '[data-js="table-car"]' ).get();
+        $tableCar.appendChild( app.createNewCar() );
+      },
+
+      createNewCar: function createNewCar() {
+        var $fragment = doc.createDocumentFragment();
+        var $tr = doc.createElement( 'tr' );
+        var $tdImage = doc.createElement( 'td' );
+        var $image = doc.createElement( 'img' );
+        var $tdBrand = doc.createElement( 'td' );
+        var $tdYear = doc.createElement( 'td' );
+        var $tdPlate = doc.createElement( 'td' );
+        var $tdColor = doc.createElement( 'td' );
+        
+        $image.setAttribute( 'src', $('[data-js="image"]').get().value);
+        $tdImage.appendChild( $image );
+
+        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
+
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+
+        return $fragment.appendChild($tr);
+      },
+
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open( 'GET', '/company.json', true );
+        ajax.send();
+        ajax.addEventListener( 'readystatechange', this.getCompanyInfo, false )
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if( !app.isReady.call( this ) )
+          return;
+
+        var data = JSON.parse( this.responseText );
+        var $companyName = $('[data-js="company-name"]').get();
+        var $companyPhone = $('[data-js="company-phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = data.phone;
+      },
+
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    }
+  })();
+
+  app.init();
+  
+})( window, document, window.DOM );
+  
